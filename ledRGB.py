51acd7a48cf801e3a3ae8ea7f82570caa61f928c
acobo2020 <a.cobo.2020@alumnos.urjc.es>
2021-10-20 09:54:02 +0200

    version final del programa

--- ledRGB.py
+++ ledRGB.py
@@ -1,6 +1,4 @@
-# ledRGB.py
-# Practica 2. Sensores y actuadores. URJC. Julio Vega
-# Codigo de ejemplo de uso del LED RGB
+
 
 import time, sys
 import RPi.GPIO as GPIO
@@ -9,21 +7,103 @@ rojoPin = 11
 azulPin = 13
 verdePin = 15
 
+
 def encender(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.HIGH)
-    
+    GPIO.output(pin, GPIO.LOW)
+
 def apagar(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-    
+    GPIO.output(pin, GPIO.HIGH)
+
 def encenderRojo():
     encender(rojoPin)
-    
-while True:
-    encenderRojo()
-    
-return
-    
+    apagar(azulPin)
+    apagar(verdePin)
+
+def encenderVerde():
+    encender(verdePin)
+    apagar(azulPin)
+    apagar(rojoPin)
+
+def encenderAzul():
+    encender(azulPin)
+    apagar(verdePin)
+    apagar(rojoPin)
+
+def encenderRosa():
+    encender(azulPin)
+    encender(rojoPin)
+    apagar(verdePin)
+
+def encenderBlanco():
+    encender(azulPin)
+    encender(rojoPin)
+    encender(verdePin)
+
+def encenderAmarillo():
+    encender(verdePin)
+    encender(rojoPin)
+    apagar(azulPin)
+
+def encenderAzulClarito():
+    GPIO.setmode(GPIO.BOARD)
+    encender(verdePin)
+    encender(azulPin)
+    apagar(rojoPin)
+
+def main():
+
+    flag = False
+    print("los colores disponibles para encender o apagar son: rojo,verde,azul,rosa,blanco,amarillo y azulClarito")
+    print("si no funciona es por que has escrito mal el comando,vuelve a intentarlo o lee el read.me")
+    while flag == False:
+        accion_usuario = input("que accion quieres realizar (encender/apagar nombreColorMostradoAntriormente o salir) ")
+        if accion_usuario == "encender rojo":
+            encenderRojo()
+        elif accion_usuario == "apagar rojo":
+            apagar(rojoPin)
+
+        if accion_usuario == "encender verde":
+            encenderVerde()
+        elif accion_usuario == "apagar verde":
+            apagar(verdePin)
+
+        if accion_usuario == "encender azul":
+            encenderAzul()
+        elif accion_usuario == "apagar azul":
+            apagar(azulPin)
+
+        if accion_usuario == "encender amarillo":
+            encenderAmarillo()
+        elif accion_usuario == "apagar amarillo":
+            apagar(rojoPin)
+            apagar(verdePin)
+
+        if accion_usuario == "encender rosa":
+            encenderRosa()
+        elif accion_usuario == "apagar rosa":
+            apagar(rojoPin)
+            apagar(azulPin)
+
+        if accion_usuario == "encender blanco":
+            encenderBlanco()
+        elif accion_usuario == "apagar blanco":
+            apagar(rojoPin)
+            apagar(azulPin)
+            apagar(verdePin)
+        if accion_usuario == "encender azulClarito":
+            encenderAzulClarito()
+        elif accion_usuario == "apagar azulClarito":
+            apagar(verdePin)
+            apagar(azulPin)
+        elif accion_usuario == "salir":
+            flag = True
+            GPIO.cleanup()
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
